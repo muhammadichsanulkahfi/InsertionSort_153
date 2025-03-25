99064f71ec857bc7e61653c26faa8f8a923094fc
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-03-26 03:16:29 +0700

    step

--- file.cpp
+++ file.cpp
@@ -54,3 +54,19 @@ j--; // step 4b
 arr[j + 1] = temp; // step 5
 }
 }
+
+void display()
+{
+cout << endl; // Output baris kosong
+cout <<"==================================" << endl;
+cout <<"total passs = " << n-1 << endl; // count element movement
+cout <<"==================================" << endl; // Output ke layar
+cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+cout <<"==================================" << endl; // Output ke layar
+
+for (int j = 0; j < n; j++)
+{ // Looping dengan j dimulai dari 0 hingga n-1
+cout << arr[j] << endl; // Output ke layar
+}
+cout << endl; // output baris kosong
+}
