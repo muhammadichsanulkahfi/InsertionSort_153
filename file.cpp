4728aef5f25b0045d59332fcd3714c49c8c586ac
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-03-26 02:57:25 +0700

    meyimpan nilai

--- file.cpp
+++ file.cpp
@@ -31,3 +31,26 @@ cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai dat
 cin >> arr[i]; // Meyimpan nilai data n kedalam array arr
 }
 }
+
+void insertionSort()
+{ // Procedure Insertionsort
+    
+int temp; // Membuat variabel data temporer atau penyimpanan sementara
+int j, i; // Membuat variabel j sebagai penanda
+
+for (i = 1; i <= n - 1; i++)
+{ // step 1
+    
+temp = arr[i]; // step 2
+
+j = i - 1; // step 3
+
+while (j >= 0 && arr[j] > temp) // step 4
+{
+arr[j + 1] = arr[j]; // step 4a
+j--; // step 4b
+}
+
+arr[j + 1] = temp; // step 5
+}
+}
